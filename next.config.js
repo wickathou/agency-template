7a04aacb9da113fb85939c306b1f05b38912cb94
Javier <javidx@gmail.com>
2023-11-26 20:02:47 +0200

    Fixed issues with app next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+const nextConfig = {
+  output: 'export'
 }
+
+
+const repo = 'agency-template'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
 }
+
+module.exports = nextConfig
+
